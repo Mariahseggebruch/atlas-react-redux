2435eeff806cbb0d8ff880f8b07765e3fa74a5c2
Mariahseggebruch <mariahseggebruch@gmail.com>
2024-09-17 10:48:24 -0500

    delete card button

---
+++ src/components/DeleteCardButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { TrashIcon } from '@heroicons/react/24/solid'; // Replace with your desired icon
+
+export const DeleteCardButton: React.FC = () => {
+  return (
+    <button className="rounded-full bg-transparent p-2 hover:bg-gray-200">
+      <TrashIcon className="h-5 w-5 text-gray-500 hover:text-gray-700" />
+    </button>
+  );
+};
